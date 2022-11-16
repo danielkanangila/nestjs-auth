e7842364a3a369b35414aa46258ed23c0c3e1f42
Daniel Kanangila <daniel.kanangila@gmail.com>
2022-11-16 02:29:42 -0700

    add user entity and create user DTO

---
+++ lib/users/typeorm/users.entity.ts
@@ -0,0 +1,86 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { Exclude, Expose } from 'class-transformer';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Index({ unique: true })
+  username: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Expose()
+  @Column({ nullable: true })
+  firstName: string;
+
+  @Expose()
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Expose()
+  @Column({ type: 'timestamp', nullable: true })
+  dateLastLoginAttempt: Date;
+
+  @Expose()
+  @Column({ type: 'timestamp', nullable: true })
+  dateLastLogin: Date;
+
+  @Expose()
+  @Index()
+  @Column({ unique: true })
+  email: string;
+
+  @Expose()
+  @Index()
+  @Column({ nullable: true, unique: true })
+  phone: string;
+
+  @Expose()
+  @Column({ type: 'boolean', default: false })
+  emailVerified: boolean;
+
+  @Expose()
+  @Column({ type: 'boolean', default: false })
+  phoneVerified: boolean;
+
+  @Exclude()
+  @Column({ nullable: true })
+  verificationToken: string;
+
+  @Expose()
+  @Column({ default: 'inactive' })
+  status: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Exclude()
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  lastUpdateAt: Date;
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, bcrypt.genSaltSync(10));
+  }
+}
